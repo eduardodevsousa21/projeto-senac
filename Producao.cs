a5461f3303c82a7d3ac6393f5cdffce0f64e6814
Ermeson Douglas <163412634+ErmesonDouglas@users.noreply.github.com>
2024-04-25 09:47:31 -0300

    producao e tarefas

---
+++ Producao.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login
+{
+    public partial class Producao : Form
+    {
+        public Producao()
+        {
+            InitializeComponent();
+        }
+
+        private void Producao_Load(object sender, EventArgs e)
+        {
+            dgvProducao.DataSource = _dbBanco.ObterTarefas();
+            dgvProducao.Columns[0].Width = 25;
+            dgvProducao.Columns[1].Width = 370;
+        }
+
+        private void dgvProducao_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            int passador = 0;
+            Login page0 = new Login();
+            page0.Show();
+            passador = 1;
+            if (passador == 1)
+            {
+                this.Hide();
+            }
+
+        }
+    }
+}
